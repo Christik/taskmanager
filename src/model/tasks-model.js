845d8fced341a824f6b545a2fd0bc029cbbd35b9
Christik <christik.lucid@gmail.com>
2022-08-13 07:10:33 +0300

    2.2. Добавляет модель для задач

---
+++ src/model/tasks-model.js
@@ -0,0 +1,9 @@
+import { generateTask } from '../mock/task.js';
+
+export default class TaskModel {
+  tasks = Array.from({ length: 12 }, generateTask);
+
+  getTasks() {
+    return this.tasks;
+  }
+}
