9e94c1bf9fdbed33aa8ca2b6c6a291de73d46406
Christina Yanovich <christik.lucid@gmail.com>
2022-08-20 17:55:24 +0300

    3.2.3. Рефакторит презентер

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -21,29 +21,14 @@ export default class BoardPresenter {
 
   #renderedTaskCount = TASK_COUNT_PER_STEP;
 
-  init(boardContainer, taskModel) {
+  constructor(boardContainer, taskModel) {
     this.#boardContainer = boardContainer;
     this.#taskModel = taskModel;
+  }
+
+  init() {
     this.#boardTasks = this.#taskModel.tasks;
-
-    render(this.#boardComponent, this.#boardContainer);
-
-    if (this.#boardTasks.every((task) => task.isArchived)) {
-      render(new NoTaskView(), this.#boardComponent.element);
-    } else {
-      render(new SortView(), this.#boardComponent.element);
-      render(this.#taskListComponent, this.#boardComponent.element);
-
-      for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
-        this.#renderTask(this.#boardTasks[i]);
-      }
-
-      if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
-        render(this.#loadMoreButtonComponent, this.#boardComponent.element);
-
-        this.#loadMoreButtonComponent.element.addEventListener('click', this.#handleLoadMoreButtonClick);
-      }
-    }
+    this.#renderBoard();
   }
 
   #renderTask(task) {
@@ -81,6 +66,27 @@ export default class BoardPresenter {
     });
 
     render(taskComponent, this.#taskListComponent.element);
+  }
+
+  #renderBoard() {
+    render(this.#boardComponent, this.#boardContainer);
+
+    if (this.#boardTasks.every((task) => task.isArchived)) {
+      render(new NoTaskView(), this.#boardComponent.element);
+    } else {
+      render(new SortView(), this.#boardComponent.element);
+      render(this.#taskListComponent, this.#boardComponent.element);
+
+      for (let i = 0; i < Math.min(this.#boardTasks.length, TASK_COUNT_PER_STEP); i++) {
+        this.#renderTask(this.#boardTasks[i]);
+      }
+
+      if (this.#boardTasks.length > TASK_COUNT_PER_STEP) {
+        render(this.#loadMoreButtonComponent, this.#boardComponent.element);
+
+        this.#loadMoreButtonComponent.element.addEventListener('click', this.#handleLoadMoreButtonClick);
+      }
+    }
   }
 
   #handleLoadMoreButtonClick = (evt) => {
