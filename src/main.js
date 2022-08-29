d7ad7b46d493a45ea937308b5fab29e2e55a7b15
Christina Yanovich <christik.lucid@gmail.com>
2022-08-29 17:53:40 +0300

    4.2.2. Подключает моковые данные фильтров

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { render } from './framework/render.js';
+import { generateFilter } from './mock/filter.js';
 import NewTaskButtonView from './view/new-task-button-view.js';
 import FilterView from './view/filter-view.js';
 import BoardPresenter from './presenter/board-presenter.js';
@@ -8,8 +9,9 @@ const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 const taskModel = new TaskModel();
 const boardPresenter = new BoardPresenter(siteMainElement, taskModel);
+const filters = generateFilter();
 
 render(new NewTaskButtonView(), siteHeaderElement);
-render(new FilterView(), siteMainElement);
+render(new FilterView(filters), siteMainElement);
 
 boardPresenter.init();
