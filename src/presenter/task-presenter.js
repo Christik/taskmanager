9725d312ecc9fd3caad2e0f2e69dc25d9938ed3f
Christina Yanovich <christik.lucid@gmail.com>
2022-09-03 12:41:19 +0300

    5.1.3. Добавляет возможность повторно инициализировать TaskPresenter

--- src/presenter/task-presenter.js
+++ src/presenter/task-presenter.js
@@ -1,4 +1,4 @@
-import { render } from '../framework/render.js';
+import { remove, render, replace } from '../framework/render.js';
 import TaskView from '../view/task-view.js';
 import TaskEditView from '../view/task-edit-view.js';
 import { isEscKeydown } from '../utils/common.js';
@@ -15,6 +15,10 @@ export default class TaskPresenter {
 
   init = (task) => {
     this.#task = task;
+
+    const prevTaskComponent = this.#taskComponent;
+    const prevTaskEditComponent = this.#taskEditComponent;
+
     this.#taskComponent = new TaskView(this.#task);
     this.#taskEditComponent = new TaskEditView();
 
@@ -22,7 +26,26 @@ export default class TaskPresenter {
 
     this.#taskEditComponent.setFormSubmitHandler(this.#handleFormSubmit);
 
-    render(this.#taskComponent, this.#taskListContainer);
+    if (prevTaskComponent === null || prevTaskEditComponent === null) {
+      render(this.#taskComponent, this.#taskListContainer);
+      return;
+    }
+
+    if (this.#taskListContainer.contains(prevTaskComponent.element)) {
+      replace(this.#taskComponent, prevTaskComponent);
+    }
+
+    if (this.#taskListContainer.contains(prevTaskEditComponent.element)) {
+      replace(this.#taskEditComponent, prevTaskEditComponent);
+    }
+
+    remove(prevTaskComponent);
+    remove(prevTaskEditComponent);
+  };
+
+  destroy = () => {
+    remove(this.#taskComponent);
+    remove(this.#taskEditComponent);
   };
 
   #replaceCardToForm = () => {
